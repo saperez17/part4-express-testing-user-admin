98c069608b216c22b4f69e2b6e62c9df6803d619
saperez17 <saperez@unicauca.edu.co>
2021-07-12 02:22:53 -0500

    Exercises 4.8 - 4.14 from b section. Testing the backend

--- controllers/blog.js
+++ controllers/blog.js
@@ -9,13 +9,51 @@ blogRouter.get('/', (request, response) => {
       })
   })
   
-blogRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+blogRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  if (typeof(body.title)==='undefined' || typeof(body.url)==='undefined'){
+    return response.status(400).end()
+  }
+
+  const blogPost = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.author,
+    likes: body.likes || 0
   })
+  try{
+    blogPost
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+  }catch(exception){
+    next(exception)
+  }
+})
+
+blogRouter.delete('/:id', async(request,response,next)=>{
+  try{
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }catch(exception){
+    next(exception)
+  }
+})
+
+blogRouter.put('/:id', async(request, response)=>{
+  const blogInDb = await Blog.findById(request.params.id)
+  const body = request.body
+
+  const blog = {
+    title: body.title || blogInDb.title,
+    author: body.author || blogInDb.author,
+    url: body.url || blogInDb.url,
+    likes: body.likes || blogInDb.likes
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new:true })
+  response.status(200).json(updatedBlog)
+})
 
 module.exports = blogRouter
